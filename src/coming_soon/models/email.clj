8a08399b6990176549494b40107ed8f97c5b79d1
Sean Johnson <sean@snootymonkey.com>
2013-03-20 06:15:07 -0400

    Configurable font colors.

--- src/coming_soon/models/email.clj
+++ src/coming_soon/models/email.clj
@@ -3,8 +3,9 @@
 ;; Regex for a valid email address
 (def email-regex #"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$")
 
-(defn valid? [email]
+(defn valid?
   "Determine if the specified email address is valid according to our email regex."
+  [email]
   (if (and (not (nil? email)) (re-matches email-regex email))
   true 
   false))
--- src/coming_soon/views/contacts.clj
+++ src/coming_soon/views/contacts.clj
@@ -19,12 +19,16 @@
                                       "font-family:" (str (config/landing-page :app-title-font) ","
                                       (config/landing-page :app-title-backup-fonts) ";")))
   [:span#app-title] (content (config/landing-page :app-title))
+  [:span#app-tagline] (set-attr :style (str "color:" (config/landing-page :app-tagline-color) ";"))
   [:span#app-tagline] (content (config/landing-page :app-tagline))
+  [:p#app-summary] (set-attr :style (str "color:" (config/landing-page :app-summary-color) ";"))
   [:p#app-summary] (content (config/landing-page :app-summary))
+  [:span#instructions] (set-attr :style (str "color:" (config/landing-page :instructions-color) ";"))
   [:span#instructions] (content (config/landing-page :instructions))
   [:input#email] (set-attr :placeholder (config/landing-page :placeholder))
   [:input#referrer] (set-attr :value referrer)
   [:button#submit] (content (config/landing-page :sign-up-btn))
+  [:p#spam-msg] (set-attr :style (str "color:" (config/landing-page :spam-msg-color) ";"))
   [:p#spam-msg] (content (config/landing-page :spam-msg)))
 
 (deftemplate admin-page "coming_soon/templates/admin.html" []
