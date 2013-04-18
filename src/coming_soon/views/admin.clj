09b204cc46add524a1301dda198661a38083d925
Sean Johnson <sean@snootymonkey.com>
2013-04-18 08:01:06 -0400

    Iterate the contacts in the admin page.

--- src/coming_soon/views/admin.clj
+++ src/coming_soon/views/admin.clj
@@ -2,4 +2,8 @@
   (:use [net.cgrand.enlive-html]))
 
 (deftemplate admin-page "coming_soon/templates/admin.html" [contacts]
-  )
+  [:table.contacts :tbody :tr] (clone-for [contact contacts]
+    [:tr first-child] (content (str (:id contact)))
+    [:tr (nth-child 2)] (content (:email contact))
+    [:tr (nth-child 3)] (content (:referrer contact))
+    [:tr last-child] (content (str (:updated-at contact)))))
