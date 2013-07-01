6525eded8557602f2f5096301ed1833173596c63
Sean Johnson <sean@snootymonkey.com>
2013-07-01 11:20:18 -0400

    Added lein ancient plugin and updated some dependencies.

--- project.clj
+++ project.clj
@@ -8,18 +8,19 @@
     [ring/ring-jetty-adapter "1.1.8"] ; Web Server https://github.com/ring-clojure/ring
     [ring-basic-authentication "1.0.2"] ; Basic HTTP/S Auth https://github.com/remvee/ring-basic-authentication
     [compojure "1.1.5"] ; Web routing http://github.com/weavejester/compojure
-    [com.taoensso/carmine "1.6.0"] ; Redis client https://github.com/ptaoussanis/carmine
+    [com.taoensso/carmine "1.12.0"] ; Redis client https://github.com/ptaoussanis/carmine
     [clj-json "0.5.3"] ; JSON encoding https://github.com/mmcgrana/clj-json/
     [org.clojure/data.xml "0.0.7"] ; XML encoding https://github.com/clojure/data.xml
     [clojure-csv/clojure-csv "2.0.0-alpha2"] ; CSV encoding https://github.com/davidsantiago/clojure-csv
     [enlive "1.1.1"] ; HTML templates https://github.com/cgrand/enlive
     [hiccup "1.0.3"] ; HTML generation https://github.com/weavejester/hiccup
-    [jayq "2.3.0"] ; ClojureScript wrapper for jQuery https://github.com/ibdknox/jayq
+    [jayq "2.4.0"] ; ClojureScript wrapper for jQuery https://github.com/ibdknox/jayq
     [tinter "0.1.1-SNAPSHOT"] ; color manipulation https://github.com/andypayne/tinter
-    [clj-time "0.5.0"] ; DateTime utilities https://github.com/clj-time/clj-time
+    [clj-time "0.5.1"] ; DateTime utilities https://github.com/clj-time/clj-time
     [expectations "1.4.49"] ; Unit testing https://github.com/jaycfields/expectations
   ]
   :plugins [
+    [lein-ancient "0.4.0"] ; Check for outdated dependencies https://github.com/xsc/lein-ancient
     [lein-ring "0.8.3"] ; Common ring tasks https://github.com/weavejester/lein-ring
     [lein-cljsbuild "0.3.2"] ; ClojureScript compiler https://github.com/emezeske/lein-cljsbuild
     [lein-expectations "0.0.7"] ; Unit testing https://github.com/gar3thjon3s/lein-expectations
