a82e50fc9c779014f9eb1fff4f99ed918fe45985
Sean Johnson <sean@snootymonkey.com>
2013-07-18 09:37:49 -0400

    Add a lein test alias that runs both expectations and cucumber features, but doesn't do a clean first.

--- project.clj
+++ project.clj
@@ -27,7 +27,10 @@
       ]
     }
   }
-  :aliases {"test!" ["do" "clean," "deps," "expectations," "cucumber"]}
+  :aliases {
+    "test" ["do" "expectations," "cucumber"]
+    "test!" ["do" "clean," "deps," "expectations," "cucumber"]
+  }
   :plugins [
     [lein-ancient "0.4.0"] ; Check for outdated dependencies https://github.com/xsc/lein-ancient
     [lein-ring "0.8.3"] ; Common ring tasks https://github.com/weavejester/lein-ring
