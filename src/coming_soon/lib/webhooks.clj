48e69d722d8086fe0a2bf8f2995efe56ed2aa4ac
Sean Johnson <sean@snootymonkey.com>
2015-08-19 17:44:41 -0400

    Cleanup core.async code.

--- src/coming_soon/lib/webhooks.clj
+++ src/coming_soon/lib/webhooks.clj
@@ -1,20 +1,16 @@
 (ns coming-soon.lib.webhooks
-  (:require [clojure.core.async :as a :refer [<!! >!! chan thread go-loop <!]]
+  (:require [clojure.core.async :as a :refer [chan sliding-buffer put! go-loop <!]]
             [coming-soon.config :as config]
             [coming-soon.webhooks.webhook :as webhook]))
 
-(def ^:private webhook-chan (chan 1000)) ; buffered channel with up to 1,000 entries without blocking
+(def ^:private webhook-chan (chan (sliding-buffer 1000))) ; buffered channel with up to 1,000 entries without loosing any
 
 (defn call
   "Send an async message for each configured webhook."
   [email referrer]
   (let [webhooks config/webhooks]
     (doseq [webhook (keys webhooks)]
-      (>!! webhook-chan {:webhook webhook :email email :referrer referrer :config (webhooks webhook)}))))
-
-(defn- call-webhook
-  [args]
-  (webhook/invoke args))
+      (put! webhook-chan {:webhook webhook :email email :referrer referrer :config (webhooks webhook)}))))
 
 ;; Dynamically require all configured webhook's namespaces
 (let [webhooks config/webhooks]
@@ -29,5 +25,5 @@
 ;; Consume the webhook channel with 100 workers
 (doseq [n (range 100)]
   (go-loop []
-    (call-webhook (<! webhook-chan))
+    (webhook/invoke (<! webhook-chan))
     (recur)))
