96dbfa4300a58aeb67e8db5569276f0a64a1bf6c
Sean Johnson <sean@snootymonkey.com>
2016-02-18 05:50:07 -0500

    Update dependencies.

--- project.clj
+++ project.clj
@@ -14,8 +14,8 @@
     [compojure "1.4.0"] ; Web routing http://github.com/weavejester/compojure
     [com.taoensso/carmine "2.12.2"] ; Redis client https://github.com/ptaoussanis/carmine
     [org.clojure/core.async "0.2.374"] ; Async programming library https://github.com/clojure/core.async/
-    [environ "1.0.1"] ; Get environment settings from different sources https://github.com/weavejester/environ
-    [clj-http "2.0.1"] ; HTTP client https://github.com/dakrone/clj-http
+    [environ "1.0.2"] ; Get environment settings from different sources https://github.com/weavejester/environ
+    [clj-http "2.1.0"] ; HTTP client https://github.com/dakrone/clj-http
     [clj-json "0.5.3"] ; JSON encoding https://github.com/mmcgrana/clj-json/
     [org.clojure/data.xml "0.1.0-beta1"] ; XML encoding https://github.com/clojure/data.xml
     [clojure-csv/clojure-csv "2.0.2"] ; CSV encoding https://github.com/davidsantiago/clojure-csv
@@ -30,7 +30,7 @@
 
   :plugins [
     [lein-ring "0.9.7"] ; Common ring tasks https://github.com/weavejester/lein-ring
-    [lein-environ "1.0.1"] ; Get environment settings from different sources https://github.com/weavejester/environ
+    [lein-environ "1.0.2"] ; Get environment settings from different sources https://github.com/weavejester/environ
   ]
 
   :profiles {
